f382a697082f681d83e8fa6f14cd828803efb08a
Vincenzo Pacella <shaduz@shaduzlabs.com>
2015-06-07 22:58:00 +0200

    euklid can be daemonized on linux

--- examples/euklid/main.cpp
+++ examples/euklid/main.cpp
@@ -24,26 +24,98 @@
 
 ----------------------------------------------------------------------------------------------------------------------*/
 
+#ifdef __linux__
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#else
+#define syslog(a,b) std::cout<<b<<std::endl;
+#endif
+
 #include <devices/DeviceMaschineMikroMK2.h>
 #include "Euklid.h"
 
 using namespace sl;
 
+//----------------------------------------------------------------------------------------------------------------------
+
+static void daemonize()
+{
+#ifdef __linux__
+ /* Our process ID and Session ID */
+  pid_t pid, sid;
+
+  /* Fork off the parent process */
+  pid = fork();
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+  /* If we got a good PID, then we can exit the parent process. */
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  /* Change the file mode mask */
+  umask(0);
+            
+  /* Open any logs here */        
+  openlog ("k-io_daemon", LOG_PID, LOG_DAEMON);
+
+  /* Create a new SID for the child process */
+  sid = setsid();
+  if (sid < 0) {
+    /* Log the failure */
+    syslog (LOG_ERROR, "[k-io] ERROR: Failed to create a new session");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Change the current working directory */
+  if ((chdir("/")) < 0) {
+    syslog (LOG_ERROR, "[k-io] ERROR: Failed to change the working directory");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Close out the open file descriptors */
+  for (int fd = sysconf(_SC_OPEN_MAX); fd>0; fd--)
+  {
+    close (fd);
+  }
+#endif
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
 int main(int argc, const char* argv[])
 {
+  daemonize();
+  syslog (LOG_NOTICE, "[k-io] started");
+
   DeviceMaschineMikroMK2 device;
   Euklid euklid(&device);
 
-  if(!euklid.connect())
+  while(true)
   {
-    std::cout << "ERROR: could not connect to device." << std::endl;
-    return -1;
+    if(!euklid.connect())
+    {
+      syslog (LOG_ERROR, "[k-io] ERROR: could not connect to device.");
+      std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+    else
+    {
+      syslog (LOG_NOTICE, "[k-io] device connected");
+      while(euklid.tick());
+      syslog (LOG_NOTICE, "[k-io] device disconnected");
+    }
   }
 
-  while (true)
-  {
-    euklid.tick();
-  }
-
+  syslog (LOG_NOTICE, "[k-io] terminated");
   return 0;
 }
+
+//----------------------------------------------------------------------------------------------------------------------
