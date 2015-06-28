ea53799cf4f3ee48e50077a6418539d07020aa69
Vincenzo Pacella <shaduz@shaduzlabs.com>
2015-06-29 00:29:00 +0200

    [midi] reworked midi class

--- test/midi/MidiMessage.cpp
+++ test/midi/MidiMessage.cpp
@@ -36,90 +36,98 @@ namespace midi
 namespace test
 {
 
-class TestMidiListener : public MidiMessageListener
+class TestMidiListenerCallbacks : public MidiMessageListener
 {
 public:
-  TestMidiListener()
+  TestMidiListenerCallbacks()
   {
-    setCallbackNoteOff(std::bind(&TestMidiListener::noteOff, this, std::placeholders::_1));
-    setCallbackNoteOn(std::bind(&TestMidiListener::noteOn, this, std::placeholders::_1));
-    setCallbackPolyPressure(std::bind(&TestMidiListener::polyPressure, this, std::placeholders::_1));
-    setCallbackControlChangee(std::bind(&TestMidiListener::controlChange, this, std::placeholders::_1));
-    setCallbackProgramChange(std::bind(&TestMidiListener::programChange, this, std::placeholders::_1));
-    setCallbackChannelPressure(std::bind(&TestMidiListener::channelPressure, this, std::placeholders::_1));
-    setCallbackPitchBend(std::bind(&TestMidiListener::pitchBend, this, std::placeholders::_1));
+    setCallbackNoteOff(std::bind(&TestMidiListenerCallbacks::noteOff, this, std::placeholders::_1));
+    setCallbackNoteOn(std::bind(&TestMidiListenerCallbacks::noteOn, this, std::placeholders::_1));
+    setCallbackPolyPressure(std::bind(&TestMidiListenerCallbacks::polyPressure, this, std::placeholders::_1));
+    setCallbackControlChangee(std::bind(&TestMidiListenerCallbacks::controlChange, this, std::placeholders::_1));
+    setCallbackProgramChange(std::bind(&TestMidiListenerCallbacks::programChange, this, std::placeholders::_1));
+    setCallbackChannelPressure(std::bind(&TestMidiListenerCallbacks::channelPressure, this, std::placeholders::_1));
+    setCallbackPitchBend(std::bind(&TestMidiListenerCallbacks::pitchBend, this, std::placeholders::_1));
   }
 
   void noteOff(tPtr<NoteOff> msg)
   {
     CHECK(msg->getType() == MidiMessage::Type::NoteOff);
-    CHECK(3 == msg->data().size());
+    CHECK(0x61 == msg->getNote());
+    CHECK(0x41 == msg->getVelocity());
+    CHECK(3    == msg->data().size());
   }
 
   void noteOn(tPtr<NoteOn> msg)
   {
     CHECK(msg->getType() == MidiMessage::Type::NoteOn);
-    CHECK(3 == msg->data().size());
+    CHECK(0x62 == msg->getNote());
+    CHECK(0x42 == msg->getVelocity());
+    CHECK(3    == msg->data().size());
   }
 
   void polyPressure(tPtr<PolyPressure> msg)
   {
     CHECK(msg->getType() == MidiMessage::Type::PolyPressure);
-    CHECK(3 == msg->data().size());
+    CHECK(0x63 == msg->getNote());
+    CHECK(0x43 == msg->getPressure());
+    CHECK(3   == msg->data().size());
   }
 
   void controlChange(tPtr<ControlChange> msg)
   {
     CHECK(msg->getType() == MidiMessage::Type::ControlChange);
-    CHECK(3 == msg->data().size());
+    CHECK(0x64 == msg->getControl());
+    CHECK(0x44 == msg->getValue());
+    CHECK(3   == msg->data().size());
   }
 
   void programChange(tPtr<ProgramChange> msg)
   {
     CHECK(msg->getType() == MidiMessage::Type::ProgramChange);
-    CHECK(2 == msg->data().size());
+    CHECK(0x65 == msg->getProgram());
+    CHECK(2    == msg->data().size());
   }
 
   void channelPressure(tPtr<ChannelPressure> msg)
   {
     CHECK(msg->getType() == MidiMessage::Type::ChannelPressure);
-    CHECK(2 == msg->data().size());
+    CHECK(0x66 == msg->getPressure());
+    CHECK(2    == msg->data().size());
   }
   void pitchBend(tPtr<PitchBend> msg)
   {
     CHECK(msg->getType() == MidiMessage::Type::PitchBend);
-    CHECK(3 == msg->data().size());
+    CHECK(0x8FF == msg->getPitch());
+    CHECK(3     == msg->data().size());
   }
 };
 
 //----------------------------------------------------------------------------------------------------------------------
 
-TEST_CASE("Test MidiMessage", "[midi]") {
+TEST_CASE("Test MidiMessageListener callbacks", "[midi]") {
 
-  NoteOff msgNoteOff(MidiMessage::Channel::Ch1, 121,61);
-  CHECK(msgNoteOff.getChannel() == MidiMessage::Channel::Ch1);
-  CHECK(msgNoteOff.getNote() == 121);
-  CHECK(msgNoteOff.getVelocity() == 61);
-  TestMidiListener midiListener;
+  TestMidiListenerCallbacks midiListener;
 
+  NoteOff msgNoteOff(MidiMessage::Channel::Ch1, 0x61,0x41);
   processMidi(&midiListener, msgNoteOff.data());
 
-  NoteOn msgNoteOn(MidiMessage::Channel::Ch2, 122,62);
+  NoteOn msgNoteOn(MidiMessage::Channel::Ch2, 0x62,0x42);
   processMidi(&midiListener, msgNoteOn.data());
   
-  PolyPressure msgPolyPressure(MidiMessage::Channel::Ch3, 123,61);
+  PolyPressure msgPolyPressure(MidiMessage::Channel::Ch3, 0x63,0x43);
   processMidi(&midiListener, msgPolyPressure.data());
 
-  ControlChange msgControlChange(MidiMessage::Channel::Ch4, 124,62);
+  ControlChange msgControlChange(MidiMessage::Channel::Ch4, 0x64,0x44);
   processMidi(&midiListener, msgControlChange.data());
 
-  ProgramChange msgProgramChange(MidiMessage::Channel::Ch5, 125 );
+  ProgramChange msgProgramChange(MidiMessage::Channel::Ch5, 0x65 );
   processMidi(&midiListener, msgProgramChange.data());
 
-  ChannelPressure msgChannelPressure(MidiMessage::Channel::Ch6, 126 );
+  ChannelPressure msgChannelPressure(MidiMessage::Channel::Ch6, 0x66 );
   processMidi(&midiListener, msgChannelPressure.data());
 
-  PitchBend msgPitchBend(MidiMessage::Channel::Ch7, 127, 67 );
+  PitchBend msgPitchBend(MidiMessage::Channel::Ch7, 0x7F, 0x11 );
   processMidi(&midiListener, msgPitchBend.data());
 
 }
