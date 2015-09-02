378a35c87757fd9c15a2b57090ee0ea551a0ce7c
Vincenzo Pacella <shaduz@shaduzlabs.com>
2015-09-02 09:04:58 +0200

    added arduino-specific header, updated arduinify.sh

--- arduinify.sh
+++ arduinify.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-rm -f arduino/k-IO.zip
-rm -rf arduino/k-IO/*
-mkdir -p arduino/k-IO
-cp -r inc/* arduino/k-IO/
-cp -r src/* arduino/k-IO/
-cp -r support/arduino/k-IO.h arduino/k-IO/k-IO.h
-rm -rf arduino/k-IO/app
-rm -rf arduino/k-IO/comm/drivers/HIDAPI
-rm -rf arduino/k-IO/comm/drivers/LibUSB
-cd arduino
+rm -f artifacts/arduino/k-IO.zip
+rm -rf artifacts/arduino/k-IO/*
+mkdir -p artifacts/arduino/k-IO
+cp -r inc/* artifacts/arduino/k-IO/
+cp -r src/* artifacts/arduino/k-IO/
+cp -r support/arduino/k-IO.h artifacts/arduino/k-IO/k-IO.h
+rm -rf artifacts/arduino/k-IO/app
+rm -rf artifacts/arduino/k-IO/comm/drivers/HIDAPI
+rm -rf artifacts/arduino/k-IO/comm/drivers/LibUSB
+cd artifacts/arduino
 find . -type f -name *.cpp -print0 | xargs -0 -I%%% mv %%% k-IO/
 zip -9 -r k-IO.zip k-IO
 cd ..
---
+++ support/arduino/k-IO.h
@@ -0,0 +1,66 @@
+/*
+        ##########    Copyright (C) 2015 Vincenzo Pacella
+        ##      ##    Distributed under MIT license, see file LICENSE
+        ##      ##    or <http://opensource.org/licenses/MIT>
+        ##      ##
+##########      ############################################################# shaduzlabs.com #####*/
+
+#pragma once
+#include <map>
+
+#include "comm/Driver.h"
+#include "devices/Device.h"
+
+#include "midi/MidiMessage.hpp"
+
+#include "devices/DeviceFactory.h"
+
+#include "comm/drivers/SAM3X8E/DriverSAM3X8E.h"
+#include "comm/drivers/SAM3X8E/DeviceHandleSAM3X8E.h"
+
+#include "util/Version.h"
+
+//#include "k-io-config.h"
+
+//--------------------------------------------------------------------------------------------------
+
+#include "comm/Transfer.h"
+
+
+//#include "devices/DeviceKompleteKontrol.cpp"
+//#include "devices/DeviceMaschineMK1.cpp"
+#include "devices/DeviceMaschineMK2.h"
+#include "devices/DeviceMaschineMikroMK2.h"
+//#include "devices/DeviceTraktorF1MK2.cpp"
+
+#include "gfx/Canvas.h"
+#include "gfx/GDisplay.h"
+#include "gfx/displays/GDisplayMaschineMikro.h"
+
+#include "util/Functions.h"
+
+//--------------------------------------------------------------------------------------------------
+/*
+namespace sl
+{
+namespace kio
+{
+
+//--------------------------------------------------------------------------------------------------
+
+class Lib
+{
+
+public:
+
+  static std::string getVersion()
+  { 
+    return util::Version(KIO_VERSION_MAJOR, KIO_VERSION_MINOR, KIO_VERSION_MICRO); 
+  }
+  
+};
+//--------------------------------------------------------------------------------------------------
+
+} // namespace kio
+} // namespace sl
+*/
