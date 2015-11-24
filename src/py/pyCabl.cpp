2b6fa05b6edc8c1d6bd5d8347d3ceb8fefcfb6b4
Vincenzo Pacella <shaduz@shaduzlabs.com>
2015-11-24 09:35:21 +0100

    python: padChanged callback

--- src/py/pyCabl.cpp
+++ src/py/pyCabl.cpp
@@ -50,6 +50,12 @@ list enumerateDevices()
 }
 //--------------------------------------------------------------------------------------------------
 
+void onPadChanged(PyObject* callable, Device::Pad pad_, uint16_t val_, bool shiftKey_)
+{
+   // Invoke callable, passing a Python object which holds a reference to x
+   boost::python::call<void>(callable, pad_, val_, shiftKey_);
+}
+
 BOOST_PYTHON_MODULE(pycabl)
 {
   enum_<DeviceDescriptor::Type>("DeviceDescriptorType")
@@ -58,6 +64,28 @@ BOOST_PYTHON_MODULE(pycabl)
         .value("HID", DeviceDescriptor::Type::HID)
         .value("Unknown", DeviceDescriptor::Type::Unknown)
         ;
+  
+#define M_PAD_CASE(nPad) value("Pad1##nPad", Device::Pad::Pad##nPad)
+
+ enum_<Device::Pad>("Pad")
+  .M_PAD_CASE( 1).M_PAD_CASE( 2).M_PAD_CASE( 3).M_PAD_CASE( 4)
+  .M_PAD_CASE( 5).M_PAD_CASE( 6).M_PAD_CASE( 7).M_PAD_CASE( 8)
+  .M_PAD_CASE( 9).M_PAD_CASE(10).M_PAD_CASE(11).M_PAD_CASE(12)
+  .M_PAD_CASE(13).M_PAD_CASE(14).M_PAD_CASE(15).M_PAD_CASE(16)
+  .M_PAD_CASE(17).M_PAD_CASE(18).M_PAD_CASE(19).M_PAD_CASE(20)
+  .M_PAD_CASE(21).M_PAD_CASE(22).M_PAD_CASE(23).M_PAD_CASE(24)
+  .M_PAD_CASE(25).M_PAD_CASE(26).M_PAD_CASE(27).M_PAD_CASE(28)
+  .M_PAD_CASE(29).M_PAD_CASE(30).M_PAD_CASE(31).M_PAD_CASE(32)
+  .M_PAD_CASE(33).M_PAD_CASE(34).M_PAD_CASE(35).M_PAD_CASE(36)
+  .M_PAD_CASE(37).M_PAD_CASE(38).M_PAD_CASE(39).M_PAD_CASE(40)
+  .M_PAD_CASE(41).M_PAD_CASE(42).M_PAD_CASE(43).M_PAD_CASE(44)
+  .M_PAD_CASE(45).M_PAD_CASE(46).M_PAD_CASE(47).M_PAD_CASE(48)
+  .M_PAD_CASE(49).M_PAD_CASE(50).M_PAD_CASE(51).M_PAD_CASE(52)
+  .M_PAD_CASE(53).M_PAD_CASE(54).M_PAD_CASE(55).M_PAD_CASE(56)
+  .M_PAD_CASE(57).M_PAD_CASE(58).M_PAD_CASE(59).M_PAD_CASE(60)
+  .M_PAD_CASE(61).M_PAD_CASE(62).M_PAD_CASE(63).M_PAD_CASE(64)
+  ;
+#undef M_PAD_CASE
   
   class_<DeviceFactory, std::shared_ptr<DeviceFactory>, boost::noncopyable >("DeviceFactory",no_init)
     .def("instance",&deviceFactory )
