69bbdbf052f7e67a51630dc1637a45ce86d3a07b
Vincenzo Pacella <shaduz@shaduzlabs.com>
2016-08-21 16:38:05 +0200

    updated sl-compiler.cmake

--- cmake-modules/sl-compiler.cmake
+++ cmake-modules/sl-compiler.cmake
@@ -5,11 +5,10 @@
         ##      ##
 ##########      ############################################################# shaduzlabs.com #######
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
-
 if( CMAKE_CXX_COMPILER_ID MATCHES "GNU|AppleClang|Clang" )
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftemplate-depth=256 -std=c++11")
+
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftemplate-depth=256 -std=c++11 -fPIC")
 
   if(CMAKE_CXX_COMPILER_ID MATCHES "AppleClang|Clang")
     foreach(flag_var CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
@@ -20,4 +19,5 @@ if( CMAKE_CXX_COMPILER_ID MATCHES "GNU|AppleClang|Clang" )
     set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -stdlib=libc++")
     set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -stdlib=libc++")
   endif()
+  
 endif()
