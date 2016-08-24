be364b6a62b1a625f4482306cc9bfb41c13e0815
Vincenzo Pacella <shaduz@shaduzlabs.com>
2016-08-24 23:31:11 +0200

    added coveralls support

--- cmake-modules/sl-ext-libs.cmake
+++ cmake-modules/sl-ext-libs.cmake
@@ -253,3 +253,16 @@ function (addlodepng)
 
   endif()
 endfunction()
+
+
+# ------------------------------------------------------------------------------------------------ #
+#  coveralls-cmake                                                                                 #
+# ------------------------------------------------------------------------------------------------ #
+function (addCoverallsCMake)
+  if(DEFINED COVERALLS_CMAKE_PATH)
+    message(STATUS "Coveralls-cmake is already available")
+  else()
+    checkout_external_project(coveralls-cmake https://github.com/JoakimSoderberg/coveralls-cmake.git master)
+    set(COVERALLS_CMAKE_PATH "${CMAKE_BINARY_DIR}/coveralls-cmake/src/coveralls-cmake/cmake" PARENT_SCOPE)
+  endif()
+endfunction()
