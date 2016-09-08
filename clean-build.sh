90a55809d4584e5390be5a23d7a3efdc724f8892
Vincenzo Pacella <shaduz@shaduzlabs.com>
2016-09-09 00:07:36 +0200

    updated build configuration

--- clean-build.sh
+++ clean-build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-rm -rf build && mkdir build && cd build && cmake .. && make && cd ..
+rm -rf build
+mkdir build
+cd build
+cmake .. -DBUILD_SHARED_LIBS=ON
+cd ..
+cmake --build build
+cmake --build build --target check
